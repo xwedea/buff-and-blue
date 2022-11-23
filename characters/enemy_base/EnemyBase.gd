a471e38282c6ad09672e55a172f759a8dafdfeb4
xwedea <mecitjiyan@gmail.com>
2022-11-22 22:17:58 -0500

    DemoEnemy script

--- characters/enemy_base/EnemyBase.gd
+++ characters/enemy_base/EnemyBase.gd
@@ -1,18 +1,9 @@
 extends "res://characters/agent_base/AgentBase.gd"
 
 
-export var isGettingHit : bool = false
-var isDead : bool = false
-
 func move(delta : float):
-	
-	if !isGettingHit:
-		$AnimationPlayer.play("idle")
-	
 	.move(delta)
 
 
 func handle_hit():
 	print("enemy got hit!")
-	isGettingHit = true
-	$AnimationPlayer.play("hit")
